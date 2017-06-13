c3ecaef6040f972428922c2fc63740109e48d784
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-06-13 19:30:27 +0300

    Command for setting base branch.

---
+++ lib/python/Q/commands/base.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*-
+from ..error import QError
+from ..settings import QSettings
+from ..command import AutoGoCommand
+from ..helper import Git
+
+
+class CommandBase(AutoGoCommand):
+    """
+    Display or change the base branch this ticket is based on.
+    """
+
+    def run(self):
+        """
+        usage: q base [<code>] [<branch>]
+        """
+        from ..q import Q
+        if len(self.args):
+            self.ticket['Base'] = self.args[0]
+            self.ticket.save()
+        base = self.ticket['Base']
+        if base:
+            base = Q.BRANCH + base + Q.END
+        else:
+            base = 'default ' + Q.BRANCH + QSettings.BASE_BRANCH + Q.END
+        self.wr("Base is " + base)
+
