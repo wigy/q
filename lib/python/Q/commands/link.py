fd7ff9f272e0027f86a87e41dec22a4194065b0a
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2020-01-02 14:37:52 +0200

    Fix project linking.

--- lib/python/Q/commands/link.py
+++ lib/python/Q/commands/link.py
@@ -15,6 +15,8 @@ class CommandLink(Command):
         usage: link <project-path>
         """
         from ..q import Q
+        from ..settings import QSettings
+
         if not len(self.args):
             self.wr(Q.TITLE+'Linked projects'+Q.END)
             if self.settings.LINKED_PROJECTS is None:
@@ -23,21 +25,25 @@ class CommandLink(Command):
                 for path in self.settings.LINKED_PROJECTS.split(':'):
                     self.wr('  ' + path)
         else:
-            # TODO: Could link all currently linked projects to the target as well.
+            settings=QSettings.load(self.args[0] + '.q')
             this_path=self.settings.__dict__['APPDIR']
-            self.settings.push()
-            self.settings.load(self.args[0] + '.q')
-            other_path=self.settings.__dict__['APPDIR']
-            if self.settings.LINKED_PROJECTS is None:
-                self.settings.LINKED_PROJECTS=this_path
-            else:
-                self.settings.LINKED_PROJECTS+=':' + this_path
-            self.settings.save(other_path + '.q')
-            self.wr('Linked ' + Q.FILE + this_path + Q.END + ' to ' + Q.FILE + other_path + Q.END)
-            self.settings.pop()
+            other_path=settings.__dict__['APPDIR']
+
             if self.settings.LINKED_PROJECTS is None:
                 self.settings.LINKED_PROJECTS=other_path
+                self.wr('Linked ' + Q.FILE + other_path + Q.END + ' to ' + Q.FILE + this_path + Q.END)
             else:
-                self.settings.LINKED_PROJECTS+=':' + other_path
+                if other_path not in self.settings.LINKED_PROJECTS.split(':'):
+                    self.settings.LINKED_PROJECTS+=':' + other_path
+                    self.wr('Linked ' + Q.FILE + other_path + Q.END + ' to ' + Q.FILE + this_path + Q.END)
             self.settings.save(this_path + '.q')
-            self.wr('Linked ' + Q.FILE + other_path + Q.END + ' to ' + Q.FILE + this_path + Q.END)
+
+            if settings.LINKED_PROJECTS is None:
+                settings.LINKED_PROJECTS=this_path
+                self.wr('Linked ' + Q.FILE + this_path + Q.END + ' to ' + Q.FILE + other_path + Q.END)
+            else:
+                if this_path not in settings.LINKED_PROJECTS.split(':'):
+                    settings.LINKED_PROJECTS+=':' + this_path
+                    self.wr('Linked ' + Q.FILE + this_path + Q.END + ' to ' + Q.FILE + other_path + Q.END)
+
+            settings.save(other_path + '.q')
